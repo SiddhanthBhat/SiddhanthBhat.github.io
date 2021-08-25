44ce22d22611abc09d67a3f492049d85725ca383
SiddhanthBhat <75930604+SiddhanthBhat@users.noreply.github.com>
2021-08-25 19:22:36 +0530

    Fixed design layout updated workflow

--- js/scripts.js
+++ js/scripts.js
@@ -52,20 +52,6 @@ $(function () {
         }
         return false;
     });
-    //TRANSITION
-    var $loader = document.querySelector(".loader");
-
-window.onload = function () {
-  $loader.classList.remove("loader--active");
-};
-
-document.querySelector(".btn").addEventListener("click", function () {
-  $loader.classList.add("loader--active");
-
-  window.setTimeout(function () {
-    $loader.classList.remove("loader--active");
-  }, 200);
-});
 
     // scroll top top
     $('.scroll-top').click(function () {
