ab0cffce9fea984ca2a3a783187a91ef0905cadc
Siddhanth Bhat <75930604+SiddhanthBhat@users.noreply.github.com>
2026-07-07 15:53:07 +0530

    yea

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// Adjust the import path to wherever you saved GlobalLoader
-import GlobalLoader from "./components/GlobalLoader"; 
-
-export default function Loading() {
-  // Next.js automatically wraps your pages in a Suspense boundary 
-  // and displays this component while the new page loads.
-  return <GlobalLoader />;
-}
